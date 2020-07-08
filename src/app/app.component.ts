5eedbb29f720e5b64d3ae456f0409dc211a65c1b
Jitesh <pawanjitesh93@gmail.com>
2020-07-08 19:02:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-mat-project';
+
+  public name: string;
+  public address: string;
+  public userList: any[];
+  public count = 0;
+  public index = 0;
+  public edited: boolean = false;
+
+  public displayedColumns: string[] = ['No.', 'Name', 'Address', 'Actions'];
+  public dataSource: any = [];
+
+  clearAll() {
+    this.name = '';
+    this.address = '';
+  }
+
+  submitAll() {
+    let object: any = {};
+    if(this.name != '' && this.address != '') {
+      object.name = this.name;
+      object.address = this.address;
+      if(this.edited) {
+        object.no = this.index + 1;
+        this.userList[this.index] = object;
+        this.edited = false;
+      } else {
+        this.count = this.count + 1;
+        object.no = this.count;
+        this.userList.push(object);
+      }
+      this.dataSource = new MatTableDataSource(this.userList);
+    }
+    console.log(this.dataSource);
+  }
+
+  onEdit(ele: any) {
+    let object: any = {};
+    this.index = ele.no - 1;
+    object = this.userList[this.index];
+    this.name = object.name;
+    this.address = object.address;
+    this.edited = true;
+  }
+
+  onDelete(ele: any) {
+    this.index = this.userList.indexOf(ele);
+    this.userList.splice(this.index, 1);
+    this.dataSource = new MatTableDataSource(this.userList);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
